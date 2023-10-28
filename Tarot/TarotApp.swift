df72e994784d70c93477c1cf7f4a42dad192228f
joytantrum <lindsayc098@gmail.com>
2023-10-28 14:08:18 -0400

    Initial Commit

---
+++ Tarot/TarotApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarotApp.swift
+//  Tarot
+//
+//  Created by Lindsay Clifford on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TarotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
